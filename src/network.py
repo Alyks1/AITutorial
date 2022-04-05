aecc61d968f3bf94d27361b994847a03ce6a72ed
Alyks1 <alexander.gemmingen@gmx.de>
2022-04-05 10:00:21 +0200

    changed cost function
    Signed-off-by: Alyks1 <alexander.gemmingen@gmx.de>

--- src/network.py
+++ src/network.py
@@ -22,8 +22,10 @@ class Network(object):
         for epoch in range(epochs):
             random.shuffle(training_data)
             mini_batches = [ training_data[i:i+mini_batch_size] for i in range(0, n, mini_batch_size)]
+            # learn
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, learning_rate)
+            # test
             if test_data:
                 print(f"Epoch {epoch}: {self.evaluate(test_data)} / {n_test}")
             else:
@@ -33,27 +35,31 @@ class Network(object):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
-        for x, y in mini_batch:
-            delta_nabla_b, delta_nabla_w = self.backprop(x,y)
+        for actual, expected in mini_batch:
+            delta_nabla_b, delta_nabla_w = self.backprop(actual,expected)
             nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
             nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
         self.biases = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
         self.weights = [w-(learning_rate/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
     
-    def backprop(self, x, y):
+    def backprop(self, actual, expected):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
 
-        activation = x
-        activations = [x]
+        # Feed Forward
+        activation = actual
+        activations = [actual]
         zs = []
 
+        # This is like feedforward fn but appends zs
         for b, w in zip(self.biases, self.weights):
-            z = np.dot(w, activation) +b
+            z = np.dot(w, activation) + b
             zs.append(z)
             activation = sigmoid(z)
             activations.append(activation)
-        delta = self.cost_derivative(activations[-1], y) * sigmoid_prime(zs[-1])
+
+        # backward pass
+        delta = self.cost_derivative(activations[-1], expected) * sigmoid_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
@@ -66,11 +72,11 @@ class Network(object):
         return (nabla_b, nabla_w)
 
     def evaluate(self, test_data):
-        test_results = [(np.argmax(self.feedforward(x)),y) for (x,y) in test_data]
-        return sum(int(x == y) for (x,y) in test_results)
+        test_results = [(np.argmax(self.feedforward(actual)), expect) for (actual,expect) in test_data]
+        return sum(int(actual == expect) for (actual,expect) in test_results)
     
-    def cost_derivative(self, output_activations, y):
-        return (output_activations - y)
+    def cost_derivative(self, output_activations, expected):
+        return pow(output_activations - expected, 2)
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
