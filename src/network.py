d7860ed4f843acc0d3642ce8c4e573d74c27df4d
Alexander Gemmingen <alexander.gemmingen@gmx.de>
2022-05-18 12:34:14 +0200

    adds comments to backprop
    Signed-off-by: Alexander Gemmingen <alexander.gemmingen@gmx.de>

--- src/network.py
+++ src/network.py
@@ -9,14 +9,14 @@ class Network(object):
         self.sizes = sizes
         self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
         self.weights = [np.random.randn(y, x) for x,y in zip(sizes[:-1], sizes[1:])]
-    
+
     def StochasticGradientDecent(self, training_data, epochs, mini_batch_size, learning_rate, test_data = None):
         # training_data is list of tuples '(training_input,expected_output)'
         if test_data: n_test = len(test_data)
         n = len(training_data)
         for epoch in range(epochs):
             random.shuffle(training_data)
-            mini_batches = [ training_data[i:i+mini_batch_size] for i in range(0, n, mini_batch_size)]
+            mini_batches = [training_data[i:i+mini_batch_size] for i in range(0, n, mini_batch_size)]
             # learn
             for mini_batch in mini_batches:
                 self.update_mini_batch(mini_batch, learning_rate)
@@ -38,7 +38,7 @@ class Network(object):
 
         self.biases = [b-(learning_rate/len(mini_batch))*nb for b, nb in zip(self.biases, nabla_b)]
         self.weights = [w-(learning_rate/len(mini_batch))*nw for w, nw in zip(self.weights, nabla_w)]
-    
+
     def backprop(self, input, expected):
         nabla_b = [np.zeros(b.shape) for b in self.biases]
         nabla_w = [np.zeros(w.shape) for w in self.weights]
@@ -56,13 +56,19 @@ class Network(object):
             activations.append(activation)
 
         # backward pass
+        # we want DC / D(w,b) so we do Dz / D(w,b) * DC / Da * Da / Dz
+        # DC / Da * Da / Dz
         delta = self.cost_derivative(activations[-1], expected) * sigmoid_derivative(zs[-1])
+        # Dz / Db = 1 * DC / Da * Da / Dz
         nabla_b[-1] = delta
+        # Dz / Dw = activation of previous layer * DC / Da * Da / Dz
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
 
         for l in range(2, self.num_layers):
             z = zs[-l]
             sd = sigmoid_derivative(z)
+            # Dz / Da = weights infront
+            # we want the DC / Da-1 = Dz / Da-1 * Da / dz * DC / Da
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sd
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
@@ -71,20 +77,20 @@ class Network(object):
     def evaluate(self, test_data):
         test_results = [(np.argmax(self.feedforward(input)), expect) for (input,expect) in test_data]
         return sum(int(actual == expect) for (actual,expect) in test_results)
-    
+
     def cost_derivative(self, output_activations, expected):
-        return output_activations - expected
+        return 2 * (output_activations - expected)
+    
+    def feedforward(self, a):
+        for bias, weight in zip(self.biases, self.weights):
+            a = sigmoid(np.dot(weight,a)+bias)
+        return a
 
 def sigmoid(z):
     return 1.0/(1.0+np.exp(-z))
 
 def sigmoid_derivative(z):
     return sigmoid(z)*(1-sigmoid(z))
-
-def feedforward(self, a):
-    for bias, weight in zip(self.biases, self.weights):
-        a = sigmoid(np.dot(weight,a)+bias)
-    return a
 
 if __name__ == '__main__':
     training_data, validation_data, test_data = mnist_loader.load_data_wrapper()
